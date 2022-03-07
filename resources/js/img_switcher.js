bf5723d1ecfb93f171f5e5604e2b867601213d6d
DreamMaker <29875719+cui0v0@users.noreply.github.com>
2022-03-08 00:22:15 +0800

    Update img_switcher.js

--- resources/js/img_switcher.js
+++ resources/js/img_switcher.js
@@ -8,25 +8,26 @@ var page
 var change =new Event('change')
     
     $(document).ready(function(){
+        initpage = main.selectedOptions[0].innerText
         page = main.selectedOptions[0].innerText
      });
-
+    
     main.addEventListener("change", function() {
         page = this.selectedOptions[0].innerText
     })
     
     pre.onclick  = function(){
-        if(page>1){
+        if(page>initpage){
             page=page-1
-            main.selectedIndex=page-1
+            main.selectedIndex=page-initpage
             main.dispatchEvent(change)
             }else{
                 alert("这已经是第一面了！")
             }
         }
     post.onclick  = function(){
-        if(page<main.options.length){
-            main.selectedIndex=page++
+        if(page<initpage-1+main.options.length){
+            main.selectedIndex=page-initpage+1
             main.dispatchEvent(change)
             }else{
             alert("这已经是最后一面了！")
